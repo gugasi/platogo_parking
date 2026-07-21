16bb6e61db78fec13448f33ca8eea303964618c5
Guram Sikharulidze <developerguram@gmail.com>
2026-07-21 10:19:32 +0200

    Task 5: Add GET /api/free-spaces endpoint for parking capacity

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   scope :api do
+    # --- NEW: Task 5 Free Spaces Route ---
+    get 'free-spaces', to: 'free_spaces#index'
+
     resources :tickets, only: [:create, :show], param: :barcode do
       resources :payments, only: [:create]
-      
-      # --- NEW: Task 4 State Route ---
       resource :state, only: [:show]
     end
   end
